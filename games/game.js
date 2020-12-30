2de85e74caf4b81ead974ab87ab7952c8fb1d2e3
David Briscoe <idbrii@gmail.com>
2020-12-30 00:12:31 -0800

    Add page for kaijux

---
+++ games/game.js
@@ -0,0 +1,20 @@
+function fullscreen() {
+    var game = document.getElementsByTagName("iframe")[0];
+    requestFullScreen(game);
+}
+
+// https://stackoverflow.com/a/7525760/79125
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
